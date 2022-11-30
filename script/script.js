759a29c26988a0901de2256baf4e58ed99b86cb8
mohamedarshath0506 <mdarsath620@gmail.com>
2022-11-30 07:38:37 +0530

    final the work

--- script/script.js
+++ script/script.js
@@ -7,7 +7,13 @@
 function time() {
   var first = new Date();
   var hour = first.getHours();
-  document.getElementById("hours").innerHTML = hour;
+  document.getElementById("hours").innerHTML = hour % 12;
+  if (hour < 12) {
+    timeing = "AM";
+  } else {
+    timeing = "PM";
+  }
+  document.getElementById("am").innerHTML = timeing;
   var minutes = first.getMinutes();
   document.getElementById("minute").innerHTML = minutes;
   var seconds = first.getSeconds();
